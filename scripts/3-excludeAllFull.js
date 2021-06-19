1653fc05ac37f1b1824e2c7ce9a7dd8747aaf6c3
Clément Aguilar <aguilar.clement@outlook.fr>
2021-06-19 15:20:05 +0200

    Update prettierrc

--- scripts/3-excludeAllFull.js
+++ scripts/3-excludeAllFull.js
@@ -2,15 +2,15 @@ const Web3 = require('web3')
 
 const { IS_PRODUCTION, CHAIN_ID, getProvider } = require('./const')
 const {
-  PFX,
-  OWNER_ADDR,
-  TEAM_TREASURY_MULTISIG_ADDR,
-  LIQUIDITY_MINING_MULTISIG_ADDR,
-  AIRDROP_ADDR,
-  TREASURY_VESTER_1,
-  TREASURY_VESTER_2,
-  TREASURY_VESTER_3,
-  TREASURY_VESTER_4,
+    PFX,
+    OWNER_ADDR,
+    TEAM_TREASURY_MULTISIG_ADDR,
+    LIQUIDITY_MINING_MULTISIG_ADDR,
+    AIRDROP_ADDR,
+    TREASURY_VESTER_1,
+    TREASURY_VESTER_2,
+    TREASURY_VESTER_3,
+    TREASURY_VESTER_4
 } = require('./governanceConstants')
 
 const pfxContract = require('../artifacts/contracts/PFX.sol/Pfx.json')
@@ -22,60 +22,60 @@ const provider = getProvider(chainId)
 const web3 = new Web3(provider)
 
 const toExclude = [
-  OWNER_ADDR,
-  TEAM_TREASURY_MULTISIG_ADDR[chainId],
-  LIQUIDITY_MINING_MULTISIG_ADDR[chainId],
-  AIRDROP_ADDR[chainId],
-  TREASURY_VESTER_1[chainId],
-  TREASURY_VESTER_2[chainId],
-  TREASURY_VESTER_3[chainId],
-  TREASURY_VESTER_4[chainId],
+    OWNER_ADDR,
+    TEAM_TREASURY_MULTISIG_ADDR[chainId],
+    LIQUIDITY_MINING_MULTISIG_ADDR[chainId],
+    AIRDROP_ADDR[chainId],
+    TREASURY_VESTER_1[chainId],
+    TREASURY_VESTER_2[chainId],
+    TREASURY_VESTER_3[chainId],
+    TREASURY_VESTER_4[chainId]
 ]
 
 const excludeAllFull = async () => {
-  const pfx = new web3.eth.Contract(pfxContract.abi, PFX[chainId])
+    const pfx = new web3.eth.Contract(pfxContract.abi, PFX[chainId])
 
-  try {
-    const accounts = await web3.eth.getAccounts()
+    try {
+        const accounts = await web3.eth.getAccounts()
 
-    console.log('Attempting to exclude all full from the account', accounts[0])
+        console.log('Attempting to exclude all full from the account', accounts[0])
 
-    toExclude.map(async (address) => await excludeFull(accounts, pfx, address))
-  } catch (error) {
-    console.error('An error occurred in excludeAllFull():\n', error)
-  }
+        toExclude.map(async (address) => await excludeFull(accounts, pfx, address))
+    } catch (error) {
+        console.error('An error occurred in excludeAllFull():\n', error)
+    }
 }
 
 excludeAllFull()
 
 async function excludeFull(accounts, pfx, address) {
-  try {
-    const accounts = await web3.eth.getAccounts()
+    try {
+        const accounts = await web3.eth.getAccounts()
 
-    console.log('Excluding', address, 'in full')
+        console.log('Excluding', address, 'in full')
 
-    await pfx.methods
-      .excludeSrc(
-        address // The account to exclude from fees as source
-      )
-      .send({
-        from: accounts[0],
-      })
+        await pfx.methods
+            .excludeSrc(
+                address // The account to exclude from fees as source
+            )
+            .send({
+                from: accounts[0]
+            })
 
-    console.log(`excludeSrc(${address}) OK`)
+        console.log(`excludeSrc(${address}) OK`)
 
-    await pfx.methods
-      .excludeDst(
-        address // The account to exclude from fees as recipient
-      )
-      .send({
-        from: accounts[0],
-      })
+        await pfx.methods
+            .excludeDst(
+                address // The account to exclude from fees as recipient
+            )
+            .send({
+                from: accounts[0]
+            })
 
-    console.log(`excludeDst(${address}) OK`)
+        console.log(`excludeDst(${address}) OK`)
 
-    console.log('Done!')
-  } catch (error) {
-    console.log('An error occurred in excludeAirdropFull():', error)
-  }
+        console.log('Done!')
+    } catch (error) {
+        console.log('An error occurred in excludeAirdropFull():', error)
+    }
 }
