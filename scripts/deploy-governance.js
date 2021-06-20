419449873526704c18021ac69988774b1e99a6e7
Clément Aguilar <aguilar.clement@outlook.fr>
2021-06-20 11:15:48 +0200

    Update deploy-governance script

--- scripts/deploy-governance.js
+++ scripts/deploy-governance.js
@@ -1,21 +1,24 @@
 const hre = require("hardhat");
+const { PFX } = require('./governanceConstants')
 
 async function main() {
   const [admin] = await hre.ethers.getSigners();
+  
+  const chainId = await admin.getChainId()
 
   const currentNonce = await admin.getTransactionCount();
 
 
-  const futureGovernorAlphaAddress = await ethers.utils.getContractAddress({
+  const futureGovernorAlphaAddress = await hre.ethers.utils.getContractAddress({
     from: admin.address,
-    nonce: currentNonce + 2 // (PFX + Timelock)
+    nonce: currentNonce + 1 // (Timelock)
   });
   console.log('futureGovernorAlphaAddress', futureGovernorAlphaAddress)
 
   // Deploy PFX, sending the total supply to the admin
-  const Pfx = await hre.ethers.getContractFactory("Pfx");
-  const pfx = await Pfx.deploy(admin.address);
-  console.log('Pfx deployed to:', pfx.address);
+  // const Pfx = await hre.ethers.getContractFactory("Pfx");
+  // const pfx = await Pfx.deploy(admin.address);
+  // console.log('Pfx deployed to:', pfx.address);
 
   // Deploy Timelock
   const TWO_DAYS_IN_SECONDS = 60 * 60 * 24 * 2;
@@ -25,7 +28,7 @@ async function main() {
 
   // Deploy GovernorAlpha
   const GovernorAlpha = await hre.ethers.getContractFactory('GovernorAlpha');
-  const governorAlpha = await GovernorAlpha.deploy(timelock.address, '0x90E487E9a08fF29B2F97ECd0eD73886692dF70D1');
+  const governorAlpha = await GovernorAlpha.deploy(timelock.address, PFX[chainId]);
   console.log('GovernorAlpha deployed to:', governorAlpha.address);
 
   console.log('Timelock admin', await timelock.admin());
